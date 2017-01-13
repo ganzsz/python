9986c1d3590fe4b996021f66a940877b3ecccb25
Joris <0935462@hr.nl>
2017-01-13 13:44:33 +0100

    revert this plox

--- ballclass.py
+++ ballclass.py
@@ -1,43 +1,49 @@
 #class voor bal om te importeren
+from paddleclass import Paddle
+
 from pyglet.gl import *
 from pyglet.window import *
-class ball:
-    def __init__(self, x, y, Xsize, Ysize, speed):
+
+class Ball:
+    def __init__(self, x, y, width, height, speed):
         self.x=x
         self.y=y
-        self.Xsize = Xsize
-        self.Ysize = Ysize
+        self.width = width
+        self.height = height
         self.speed = speed
         self.dy = speed
         self.dx = speed
 
     def draw(self):
         pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', 
-            [self.x, self.y, self.x-self.Xsize, self.y, self.x-self.Xsize, self.y-self.Ysize, self.x, self.y-self.Ysize]))
+            [self.x, self.y, self.x-self.width, self.y, self.x-self.width, self.y-self.height, self.x, self.y-self.height]))
 
-    def update(self, window, paddleR, paddleL):
+    def update(self, window, paddleLeft, paddleRight):
         if(self.x>=window.width):
             self.dx=-self.speed
             print('right')
-        if(self.x<=self.Xsize):
+        if(self.x<=self.width):
             self.dx=self.speed
             print('left')
         if(self.y>=window.height):
             self.dy=-self.speed
             print('top')
-        if(self.y<=self.Ysize):
+        if(self.y<=self.height):
             self.dy=self.speed
             print('bot')
 
-        if(self.x>=paddleR.left and ((self.y<paddleR.position+(paddleR.height/2) and self.y>paddleR.position-(paddleR.height/2)) 
-                                  or (self.y<paddleR.position+self.Ysize+(paddleR.height/2) and self.y>paddleR.position+self.Ysize-(paddleR.height/2)))):
-            self.dx=-self.speed
-            print('paddle right')
-        if(self.x-self.Xsize<=paddleL.left+paddleL.width 
-                                and ((self.y<paddleL.position+(paddleL.height/2) and self.y>paddleL.position-(paddleL.height/2)) 
-                                  or (self.y<paddleL.position+self.Ysize+(paddleL.height/2) and self.y>paddleL.position+self.Ysize-(paddleL.height/2)))):
-            self.dx=self.speed
-            print('paddle left')
+        if paddleLeft.left + paddleLeft.width < self.x - self.width:
+            if paddleLeft.left + paddleLeft.width >= self.x - self.width + self.dx and (self.y - self.height <= paddleLeft.position + paddleLeft.height/2 <= self.y or self.y <= paddleLeft.position - paddleLeft.height/2 <= self.y - self.height):
+                self.dx *= -1
+                self.speed += .5
+                paddle.speed += .4
+                print('paddle left hit')
+        if paddleRight.left > self.x:
+            if paddleRight.left <= self.x + self.dx and (self.y - self.height <= paddleRight.position + paddleRight.height/2 <= self.y or self.y <= paddleRight.position - paddleRight.height/2 <= self.y - self.height):
+                self.dx *= -1
+                self.speed += .5
+                paddle.speed += .4
+                print('paddle right hit')
 
         self.y+=self.dy
         self.x+=self.dx
