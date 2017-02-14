5fc2d0205dd6ee1080bd177a98f68110a4a14052
Hans de Rooij <0912423@hr.nl>
2017-02-14 11:30:27 +0100

    How do you like this scoreboard

--- textboxclass.py
+++ textboxclass.py
@@ -1,4 +1,4 @@
-from gameobjectclass import GameObject
+from abstractobjects import GameObject
 
 from pyglet.gl import *
 from pyglet.window import *
@@ -6,18 +6,32 @@ from pyglet.window import *
 class textBox(GameObject):
     text = "Hoi"
     def draw(self):
-        pyglet.text.Label(self.text,
+        pyglet.text.Label(str(self.text),
                           font_name='Comic Sans',
                           font_size=36,
                           x=self.centerX, y=self.centerY,
                           anchor_x='center', anchor_y='center').draw()
 
-class score:
+    def setText(self, text):
+        self.text = text
+        print(text)
+
+class score(GameObject):
+    def __init__(self, centerX, centerY, height, width):
+        super().__init__(centerX, centerY, height, width)
+        self.textOne = textBox(self.centerX-100, self.centerY, self.height, self.width)
+        self.textTwo = textBox(self.centerX+100, self.centerY, self.height, self.width)
+        self.textOne.text = 0
     playerOne = 0
     playerTwo = 0
     def scorePlayerOne(self):
-        self.playerOne++
+        self.playerOne+=1
+        self.textOne=self.playerOne
 
     def scorePlayerTwo(self):
-        self.playerTwo++
+        self.playerTwo+=1
+        self.textTwo.text = self.playerTwo
 
+    def draw(self):
+        self.textOne.draw()
+        self.textTwo.draw()
